212684535b9045c51d583c13d375d7217d57f88a
Adam Fučík <155400992+bababooex@users.noreply.github.com>
2025-07-23 12:26:36 +0200

    Update new_codes.py

--- tv-b-gone/new_codes.py
+++ tv-b-gone/new_codes.py
@@ -1,4 +1,4 @@
-# Original file from https://github.com/bikeNomad/micropython-tv-b-gone
+# Some codes caused pigpio to crash, so I decided to not use them, maybe I will modify this in the future
 # [name], on_us, off_us, on_us, off_us,...
 CODES=[
     ('NEC', 8854, 4517, 580, 511, 549, 557, 577, 1650, 575, 533, 576, 512, 547, 542, 549, 556, 549, 543, 547, 1663, 577, 1636, 577, 513, 574, 1638, 578, 1634, 580, 1632, 578, 1635, 578, 1636, 576, 514, 578, 510, 577, 530, 577, 1650, 578, 530, 580, 509, 576, 514, 577, 514, 579, 1648, 579, 1650, 579, 1649, 581, 509, 580, 1632, 580, 1632, 580, 1632, 581, 1629, 584, 40716, 8913, 2284, 578, 95789, 8878, 2318, 552),
