f41d512ce883b2e1b2e791d9434d8ec8886234b6
Adam Fučík <155400992+bababooex@users.noreply.github.com>
2025-07-26 11:25:32 +0200

    Add files via upload
    Added jamming file

---
+++ tv-b-gone/IR-jam.py
@@ -0,0 +1,23 @@
+import time
+import pigpio
+
+# === Configuration ===
+GPIO_IR_LED = 17
+# =====================
+
+pi = pigpio.pi()
+pi.set_mode(GPIO_IR_LED, pigpio.OUTPUT)
+frequency = 38000
+pi.set_PWM_range(GPIO_IR_LED, 255)
+duty_cycle = 128
+pi.set_PWM_dutycycle(GPIO_IR_LED, duty_cycle)
+pi.set_PWM_frequency(GPIO_IR_LED, frequency)
+if __name__ == "__main__":
+     try:
+        while True:
+           time.sleep(1)
+     except KeyboardInterrupt:
+        print("\nInterrupted by user.")
+     finally:
+         pi.set_PWM_dutycycle(GPIO_IR_LED, 0)
+         pi.stop()
