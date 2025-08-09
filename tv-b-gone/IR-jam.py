9151f7dbed8e937408c02e3b2e2ae4b38e7c4739
Adam Fučík <155400992+bababooex@users.noreply.github.com>
2025-08-09 14:17:53 +0200

    Update IR-jam.py

--- tv-b-gone/IR-jam.py
+++ tv-b-gone/IR-jam.py
@@ -1,20 +1,31 @@
+import sys
 import time
 import pigpio
-# This python script just sends blank 38kHz signal, just like the old-school NE555 IR jammer
-# === Configuration ===
-GPIO_IR_LED = 17
-# =====================
-pi = pigpio.pi()
-pi.set_mode(GPIO_IR_LED, pigpio.OUTPUT)
-pi.set_PWM_range(GPIO_IR_LED, 255)
-pi.set_PWM_dutycycle(GPIO_IR_LED, 128)
-pi.set_PWM_frequency(GPIO_IR_LED, 38000)
-if __name__ == "__main__":
-     try:
+# Better version with  variable frequency and predefined gpio pin in bash script
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: IR-jam.py <frequency> <gpio_pin>")
+        sys.exit(1)
+
+    gpio_pin = int(sys.argv[2])
+    frequency = int(sys.argv[1])
+
+    pi = pigpio.pi()
+
+    pi.set_mode(gpio_pin, pigpio.OUTPUT)
+    pi.set_PWM_range(gpio_pin, 255)
+    pi.set_PWM_dutycycle(gpio_pin, 128)  # 50% DCL
+    pi.set_PWM_frequency(gpio_pin, frequency)
+
+    try:
+        print(f"Sending {frequency} square wave PWM. Press Ctrl+C to stop.")
         while True:
-           time.sleep(1)
-     except KeyboardInterrupt:
+            time.sleep(1)
+    except KeyboardInterrupt:
         print("\nInterrupted by user.")
-     finally:
-         pi.set_PWM_dutycycle(GPIO_IR_LED, 0)
-         pi.stop()
+    finally:
+        pi.set_PWM_dutycycle(gpio_pin, 0)
+        pi.stop()
+
+if __name__ == "__main__":
+    main()
